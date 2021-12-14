785f3cf3b0d6efc781d6cc4371e2acd27a2193a2
Ivan Savenko <mortlilport@mail.ru>
2021-12-14 15:44:59 +0300

    adding a third paragraph

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const welcomeName = () => {
+console.log('Welcome to the Brain Games!');
+
+const name = readlineSync.question('May I have your name? ');
+
+console.log(`Hello, ${name}!`);
+};
+
+export default welcomeName;
