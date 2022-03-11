78fc802002acad03db38d7b970a43af26096259f
Ivan Savenko <mortlilport@mail.ru>
2022-03-11 19:54:22 +0300

    fix lint

--- src/games/calc.js
+++ src/games/calc.js
@@ -9,10 +9,10 @@ const calcGame = () => {
     const secondOperand = Math.floor(Math.random() * 100);
     const choiceOperator = Math.floor(Math.random() * 3);
     let correctAnswer = 0;
-    let question = `${firstOperand} ${ArrayOfOperators[choiceOperator]} ${secondOperand}`;
+    const question = `${firstOperand} ${ArrayOfOperators[choiceOperator]} ${secondOperand}`;
 
     if (ArrayOfOperators[choiceOperator] === '+') {
-	  correctAnswer = firstOperand + secondOperand;
+      correctAnswer = firstOperand + secondOperand;
     }
     if (ArrayOfOperators[choiceOperator] === '*') {
       correctAnswer = firstOperand * secondOperand;
