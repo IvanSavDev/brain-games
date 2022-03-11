7cf379a11bfadeca9b6cfd3908cca1c0ab6861e8
Ivan Savenko <mortlilport@mail.ru>
2022-03-11 21:41:10 +0300

    fix lint

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,7 +1,7 @@
 import gameStructure from '../index.js';
 
 const searchGcd = () => {
-  const rules = 'Find the greatest common divisor of given numbers.'
+  const rules = 'Find the greatest common divisor of given numbers.';
 
   const game = () => {
     const firstOperand = Math.floor(Math.random() * 100);
@@ -9,17 +9,17 @@ const searchGcd = () => {
     const question = `${firstOperand} ${secondOperand}`;
     const minOperand = firstOperand > secondOperand ? secondOperand : firstOperand;
     let correctAnswer = 0;
-    for (let i = minOperand; i > 0; i--) {
-      if (firstOperand % i == 0 && secondOperand % i == 0) {
+    for (let i = minOperand; i > 0; i -= 1) {
+      if (firstOperand % i === 0 && secondOperand % i === 0) {
         correctAnswer = i;
         break;
       }
     }
 
     return [question, correctAnswer];
-  }
+  };
 
   gameStructure(rules, game);
-}
+};
 
 export default searchGcd;
